7842a4535acb76ed2cd2a7da7cd10fe5caf1547c
Scott Lowe <scott.lowe@scottlowe.org>
2017-10-18 08:59:59 -0600

    Update formatting
    Update formatting of all Terraform configurations per `terraform fmt`
    
    Signed-off-by: Scott Lowe <scott.lowe@scottlowe.org>

--- terraform/aws/asg-elb/data.tf
+++ terraform/aws/asg-elb/data.tf
@@ -1,16 +1,18 @@
 data "aws_ami" "atomic_ami" {
-    most_recent             = true
-    owners                  = ["410186602215"]
-    filter {
-        name                = "name"
-        values              = ["CentOS Atomic Host 7*"]
-    }
-    filter {
-        name                = "virtualization-type"
-        values              = ["hvm"]
-    }
+  most_recent = true
+  owners      = ["410186602215"]
+
+  filter {
+    name   = "name"
+    values = ["CentOS Atomic Host 7*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 data "aws_vpc" "default_vpc" {
-    default                 = true
+  default = true
 }
