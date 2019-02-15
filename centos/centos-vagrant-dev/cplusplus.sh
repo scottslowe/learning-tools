5c7ff653dfc9dfc53ca84deef3edb9a3cf6ce9ec
huangzhaorongit <huangzhaorongit@126.com>
2019-02-15 13:13:58 +0800

    clear

--- centos/centos-vagrant-dev/cplusplus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [  ! -f /home/vagrant/.vagrant-installation/cplusplus.installed ]
-then
-	echo Installing C/C++ dependencies
-    yum -y install clang
-    yum -y install cmake
-    yum -y install valgrind
-    touch /home/vagrant/.vagrant-installation/cplusplus.installed
-fi
